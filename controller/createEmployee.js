9ccb5f1e6f55990961b71957edebc1047e10b745
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2020-01-13 22:05:09 +0200

    employee registration with database

--- controller/createEmployee.js
+++ controller/createEmployee.js
@@ -1,8 +1,10 @@
-import emp from '../model/data';
-import mngrvalidation from '../helper/EmpValidation';
-const createEmp = (req, res)=>{
-    try{
-        const {error} =mngrvalidation.validation(req.body);
+import mngrvalidation from '../helper/empValidation';
+import Database from '../database/connection';
+import { date } from 'joi';
+const db = new Database();
+const createEmp = async (req, res)=>{
+     
+        const {error} = mngrvalidation.validation(req.body);
         if(error)
         {
             return res.status(400).json({
@@ -11,37 +13,41 @@ const createEmp = (req, res)=>{
                 
             });
         }
-        const id = emp.length +1;
-    const newEmp = {
-        id:id,
-        emp_name :req.body.emp_name,
-        national_id:req.body.national_id,
-        phone_number:req.body.phone_number,
-        email:req.body.email,
-        DOB:req.body.DOB,
-        status:req.body.status,
-        postion:req.body.postion
-    }
-    emp.push(newEmp);
-    const empl = req.body;
-    res.status(201).send({
-        status:"Employee successfully created",
-        data:{
-            id,
-            emp_name:newEmp.emp_name,
-            national_id:newEmp.national_id,
-            phone_number:newEmp.phone_number,
-            email:newEmp.email,
-            DOB:newEmp.DOB,
-            status:newEmp.status,
-            postion:newEmp.postion
-        }
-
+        try{
+            const existing_email = req.body.email;
+            const { rowCount } = await db.query('SELECT email FROM managers WHERE email = $1', [existing_email]);
         
-    })
+            if (rowCount) {
+              return res.status(409).json({
+                status: 409,
+                error: ' Your email had already used. use another to proceed ',
+              });
+            }
+        //const created_on = new date();
+        const newEmp = [
+            req.body.employeeName,
+            req.body.nationalID,
+            req.body.phoneNumber,
+            req.body.email,
+            req.body.dateOfBirth,
+            req.body.status,
+            req.body.position,
+        ];
+        const queryInsert = "INSERT INTO employees(employeeName,nationalID,phoneNumber,email,dateOfBirth,status,position) VALUES($1,$2,$3,$4,$5,$6,$7) returning *";
+    
+   const {rows} = await db.query(queryInsert, newEmp);
+   if (!rows) return res.status(500).send("Employee not recorded")
+   res.status(201).json({
+       status:201,
+       data:rows[0]
+   })
 }
 catch (err){
-    return err;
+    res.status(500).json({
+        status:500,
+        error:"Internal server error occured"
+    });
+    console.log(err);
 }
-}
+};
 export default createEmp;
