392af037698d2f5f872d158cd264c32cec98d1e3
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2020-01-14 01:42:53 +0200

    update server file

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// const express = require('express');
 import express from 'express';
-// const router = require('./routes/route');
 import router from './routes/route';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(router);
-// module.exports = app;
 export default app;
