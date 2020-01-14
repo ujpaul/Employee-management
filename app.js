e6229bef2913ebb2d17c46dc45454d55009ad9bc
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2020-01-14 19:38:21 +0200

    setting up swagger in index file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import swagger_ui from 'swagger-ui-express';
 import router from './routes/route';
+import swagger from './swagger.json';
+
 const app = express();
 app.use(express.json());
+app.use('/swagger',swagger_ui.serve,swagger_ui.setup(swagger));
 app.use(express.urlencoded({extended:true}));
 app.use(router);
 export default app;
