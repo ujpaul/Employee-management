f41d0f6c1755639bce2d3b89df1c00b6db2fd6d2
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2020-01-14 12:28:25 +0200

    activate employee

--- controller/activate.js
+++ controller/activate.js
@@ -1,17 +1,43 @@
-import employees from '../model/data';
-const activate =(req, res)=>{
-    const emp = employees.find((em) => em.id === parseInt(req.params.id));
-    if(!emp)
+import Database from '../database/connection';
+const db = new Database;
+const activate = async(req, res)=>{
+    try{
+        const id= req.params.id;
+        const {rows}= await db.query('SELECT id FROM employees');
+    const empId = rows.find((em) => em.id === parseInt(id,10));
+    if(!empId)
     {
         res.status(404).json({
             status:404,
             error:"employee you are looking for not found"
         })
     }
-    emp.status = "active";
-    res.status(200).json({
-        status:200,
-        message:"employee Activated successfully"
-    })
+    if(empId)
+    {
+        const employee = ["active"];
+        const update = 'UPDATE employees SET status=$1 returning *';
+        const {rows} = await db.query(update,employee,);
+        if(rows)
+        {
+            res.status(200).json({
+                status:200,
+                message:"employee Activated",
+                Employee:rows[0],
+            });
+        }
+    }
+   
+    
+    }
+    catch(err)
+    {
+        res.status(500).json({
+            status:500,
+            Error:"Internal server error occured"
+        })
+        console.log(err);
+    }
+    
+   
 }
 export default activate;
