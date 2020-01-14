5458f73ad83d89529f71a647a48f33e4e8732b34
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2020-01-14 02:47:37 +0200

    add file to log manager in

--- controller/signin.js
+++ controller/signin.js
@@ -1,24 +1,50 @@
-import managers from '../model/manager';
-import auth from '../middleware/auth'
-const signin = (req, res)=>{
-    try{
-    const manager =managers.find(m=>m.username ===req.body.email && m.password === req.body.password);
-    //const token =req.header("x-auht-header");
-    if(!manager)
-    {
-        res.status(400).json({
-            status:400,
-            error:"Invalid username or password"
-        })
-    }
-    res.status(200).json({
-        status:200,
-        message:"Login successfully",
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import Database from '../database/connection';
 
-    })
-    }catch (err){
-        return err;
+const db = new Database();
+dotenv.config();
+
+const Signin = async (req, res) => {
+  try {
+    const managerEmail = req.body.email;
+    const managerPassword = req.body.password;
+    const { rows } = await db.query('SELECT * FROM managers WHERE email = $1', [managerEmail]);
+    if (!rows[0]) {
+      return res.status(401).json({
+        status: 401,
+        error: 'Wrong credentials',
+
+      });
     }
-    console.log(manager);
-}
-export default signin;
+    const verifypassword = await bcrypt.compare(managerPassword.trim(), rows[0].password);
+    if (!verifypassword) {
+      return res.status(401).json({
+        status: 401,
+        error: 'Wrong credentials',
+
+      });
+    }
+
+    const Payload = {
+        nationalId: rows[0].nationalId,
+      phoneNumber: rows[0].phoneNumber,
+    };
+
+    const token = await jwt.sign(Payload, process.env.JWT_KEY, { expiresIn: '1d' });
+
+    return res.status(200).json({
+      status: 200,
+      message: 'Login successfully',
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  return {};
+};
+
+export default Signin;
